b44386878a3239fd21cfc5be4780d49b7ce7d588
murfi351 <!ford12*>
2015-10-15 20:39:46 -0500

    Looked into accelerometer. Tried to determine how the event listener worked.

--- Beta/ftc_app-DR/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/DR_Tank_Test.java
+++ Beta/ftc_app-DR/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/DR_Tank_Test.java
@@ -5,8 +5,9 @@ import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorManager;
-
+import android.hardware.SensorEventListener;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.AccelerationSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.GyroSensor;
 import com.qualcomm.robotcore.hardware.TouchSensor;
@@ -20,20 +21,29 @@ import com.qualcomm.robotcore.hardware.ColorSensor;
 /**
  * Created by steve.brooks on 9/30/2015.
  */
-public class DR_Tank_Test extends OpMode {
+public class DR_Tank_Test extends OpMode implements SensorEventListener{
 
-    private static final float NS2S = 1.0f / 1000000000.0f;
-    private final float[] deltaRotationVector = new float[4];
-    private float timestamp;
+    public SensorManager mSensorManager;
+    public Sensor Accelerometer;
+
+    public float deltaX = 0;
+    public float deltaY = 0;
+    public float deltaZ = 0;
+    public float deltaXMax = 0;
+    public float deltaYMax = 0;
+    public float deltaZMax = 0;
+    public float lastX, lastY, lastZ;
 
     ColorSensor sensorRGB;
     DcMotor motorRight;
     DcMotor motorLeft;
-    //Initialize the Gyro Sensor
+    //Initialize the Accelerometer
     SensorManager sensorService = (SensorManager) hardwareMap.appContext.getSystemService(Context.SENSOR_SERVICE);
     {
-        Sensor Gyro_Sensor = sensorService.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        Sensor Accelerometer = sensorService.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        SensorManager.registerListener(this, Accelerometer , SensorManager.SENSOR_DELAY_NORMAL);
     }
+
 //TouchSensor touchSensor;
    // Servo servo1;
 
@@ -59,8 +69,7 @@ public class DR_Tank_Test extends OpMode {
         //servo1Delta = 0.1;
 
     }
-
-    public void onSensorChanged(SensorEvent event) { //when sensor changes, calculate
+   /* public void onSensorChanged(SensorEvent event) { //when sensor changes, calculate
         if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
 
         if (timestamp != 0) {
@@ -93,10 +102,13 @@ public class DR_Tank_Test extends OpMode {
 
         telemetry.addData("X-Axis", deltaRotationMatrix[0]);
         telemetry.addData("Y-Axis", deltaRotationMatrix[1]);
-        telemetry.addData("Z-Axis", deltaRotationMatrix[2]);
+        telemetry.addData("Z-Axis", deltaRotationMatrix[2]); */
 
 
-        }
+       //}
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
     public void loop() {
         float throttlel = gamepad1.left_stick_y;
         float throttler = gamepad1.right_stick_y;
@@ -104,14 +116,12 @@ public class DR_Tank_Test extends OpMode {
         throttler = Range.clip(throttler, -1, 1);
 
         throttlel = (float)scaleInput(throttlel);
-        throttler = (float)scaleInput(throttler);
+        throttler = (float) scaleInput(throttler);
 
         motorLeft.setPower(throttlel);
         motorRight.setPower(throttler);
 
-
-
-
+        onSensorChanged();
         //servo1.setPosition(servo1Position);
 
 
@@ -141,6 +151,20 @@ public class DR_Tank_Test extends OpMode {
                     + motorRight.getCurrentPosition());
         //telemetry.addData("Is Pressed", String.valueOf(touchSensor.isPressed()) );
     }
+@Override
+        public void onSensorChanged(SensorEvent event) {
+            // get the change of the x,y,z values of the accelerometer
+            deltaX = Math.abs(lastX - event.values[0]);
+            deltaY = Math.abs(lastY - event.values[1]);
+            deltaZ = Math.abs(lastZ - event.values[2]);
+            // if the change is below 2, it is just plain noise
+            if (deltaX < 2)
+                deltaX = 0;
+            if (deltaY < 2)
+                deltaY = 0;
+            if (deltaZ < 2)
+                deltaZ = 0;
+        }
     double scaleInput(double dVal) {
         double[] scaleArray = {0.0, 0.05, 0.09, 0.10, 0.12, 0.15, 0.18, 0.24,
                 0.30, 0.36, 0.43, 0.50, 0.60, 0.72, 0.85, 1.00, 1.00};
