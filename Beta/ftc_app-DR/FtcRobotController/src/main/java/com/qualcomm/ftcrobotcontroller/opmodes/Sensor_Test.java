71b163e4db7387f52ced24fb9df4d17f78a2f57e
LauraAEvans <elzagirl10@gmail.com>
2016-01-15 14:20:54 -0600

    Created a linefollow method that still needs to be debugged.

--- Beta/ftc_app-DR/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Sensor_Test.java
+++ Beta/ftc_app-DR/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Sensor_Test.java
@@ -15,7 +15,6 @@ public class Sensor_Test extends OpMode{
 
     OpticalDistanceSensor distanceSensor;
     ColorSensor colorSensor;
-    boolean flag = true;
 
     public void init() {
 
@@ -23,33 +22,49 @@ public class Sensor_Test extends OpMode{
         colorSensor = hardwareMap.colorSensor.get("Sensor_color");
     }
     public void loop () {
+        double ODSr = distanceSensor.getLightDetectedRaw();
+        double ODS = distanceSensor.getLightDetected();
 
-        if(flag) {
-
-            double ODSr = distanceSensor.getLightDetectedRaw();
-            double ODS = distanceSensor.getLightDetected();
-
-            float hsvValues[] = {6F, 6F, 6F};
+        if (gamepad1.x == false)
+        {
+            colorSensor.enableLed(true);
+            float hsvValues[] = {6F,6F,6F};
             final float values[] = hsvValues;
             final View relativeLayout = ((Activity) hardwareMap.appContext).findViewById(R.id.RelativeLayout);
 
-            Color.RGBToHSV(colorSensor.red() * 8, colorSensor.green() * 8, colorSensor.blue() * 8, hsvValues);
+            Color.RGBToHSV(colorSensor.red()*8, colorSensor.green()*8, colorSensor.blue()*8, hsvValues);
 
             telemetry.addData("Object Reference ", colorSensor.toString());
             telemetry.addData("Clear", colorSensor.alpha());
             telemetry.addData("Red  ", colorSensor.red());
             telemetry.addData("Blue", colorSensor.blue());
             telemetry.addData("Green", colorSensor.green());
-            telemetry.addData("Hue", hsvValues[0]);
-
-            telemetry.addData("Distance_sensor_output", ODS);
-            telemetry.addData("Distance_sensor_output_Raw", ODSr);
-
-            relativeLayout.post(new Runnable() {
-                public void run() {
-                    relativeLayout.setBackgroundColor(Color.HSVToColor(0xff, values));
-                }
-            });
         }
+        else
+        {
+            colorSensor.enableLed(false);
+        }
+
+        float hsvValues[] = {6F,6F,6F};
+        final float values[] = hsvValues;
+        final View relativeLayout = ((Activity) hardwareMap.appContext).findViewById(R.id.RelativeLayout);
+
+        Color.RGBToHSV(colorSensor.red()*8, colorSensor.green()*8, colorSensor.blue()*8, hsvValues);
+
+        telemetry.addData("Object Reference ", colorSensor.toString());
+        telemetry.addData("Clear", colorSensor.alpha());
+        telemetry.addData("Red  ", colorSensor.red());
+        telemetry.addData("Blue", colorSensor.blue());
+        telemetry.addData("Green", colorSensor.green());
+        telemetry.addData("Hue", hsvValues[0]);
+
+        telemetry.addData("Distance_sensor_output", ODS);
+        telemetry.addData("Distance_sensor_output_Raw", ODSr);
+
+        relativeLayout.post(new Runnable() {
+            public void run() {
+                relativeLayout.setBackgroundColor(Color.HSVToColor(0xff, values));
+            }
+        });
     }
 }
